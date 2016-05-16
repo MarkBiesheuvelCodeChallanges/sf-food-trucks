3b58a71b735b3de0a723334952f07efdca35a01e
Mark Biesheuvel <mail@markbiesheuvel.nl>
2016-05-16 22:32:45 +0200

    Create DynamoDB and insert a few rows to get started

--- import.js
+++ import.js
@@ -1,3 +1,10 @@
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+    region: "eu-central-1"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
 
 // This is a local copy of https://data.sfgov.org/api/views/rqzj-sfat/rows.json?accessType=DOWNLOAD
 // TODO: Redownload on import
@@ -12,12 +19,43 @@ dataset.meta.view.columns.forEach((column, i) => {
     mapping[column.fieldName] = i;
 });
 
-dataset.data.forEach((row) => {
+const insertItem = (row) => {
 
-    // TODO: insert into database
-    console.log(row[mapping.applicant]);
-    console.log(row[mapping.facilitytype]);
-    console.log(row[mapping.latitude]);
-    console.log(row[mapping.longitude]);
+    let objectid = parseInt(row[mapping.objectid], 10); // Integer
 
-});
+    let applicant = row[mapping.applicant]; // String
+    let facilitytype = row[mapping.facilitytype]; // String
+    let status = row[mapping.status]; // String
+
+    let fooditems = row[mapping.fooditems].split(/\s*:\s*/); // List of strings
+
+    let lat = parseFloat(row[mapping.latitude]); // Float
+    let lng = parseFloat(row[mapping.longitude]); // Float
+
+    let params = {
+        TableName: "FoodTrucks",
+        Item: {
+            objectid,
+            applicant,
+            facilitytype,
+            status,
+            fooditems,
+            coordinates: {lat, lng}
+        }
+    };
+
+    docClient.put(params, function (err, data) {
+        if (err) {
+            throw err;
+        }
+    });
+
+};
+
+// Test on a four items for now
+insertItem(dataset.data[0]);
+insertItem(dataset.data[1]);
+insertItem(dataset.data[2]);
+insertItem(dataset.data[3]);
+// dataset.data.forEach(insertItem);
+
